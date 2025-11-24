c577ceb0c23ea61883ac2678a1bf95b21179dd14
Mirai Day <55056986+AD417@users.noreply.github.com>
2025-11-24 18:15:47 -0500

    Allow the tokens to be spread out in a circle

--- src/sideMenu/SideButtons.tsx
+++ src/sideMenu/SideButtons.tsx
@@ -17,6 +17,42 @@ function shuffleTokens(tokens: TokenData[]): TokenData[] {
     });
 }
 
+function spreadTokens(token: TokenData[]): TokenData[] {
+    const total = token.length;
+
+    const center = { 
+        y: document.documentElement.scrollHeight / 2, 
+        x: document.documentElement.scrollWidth / 2,
+    };
+
+    const radius = Math.min(center.y, center.x) - 150;
+    if (radius < 0) return token;
+    
+    const angleSeperation = Math.PI * 2 / total;
+    
+    const list = token.map((token, index) => {
+        return {
+            angle: Math.atan2(token.position.top + 75 - center.y, token.position.left + 75 - center.x),
+            index,
+            id: token.id
+        };
+    });
+
+    console.log(list)
+    console.log(list.sort(({angle: a1}, {angle: a2}) => a2 - a1))
+
+    return list.sort(({angle: a1}, {angle: a2}) => a2 - a1).map(({index}, i) => {
+        const angle = angleSeperation * (-(total - 1) / 2 + i);
+        return {
+            ...token[index],
+            position: {
+                top: center.y + radius * -Math.sin(angle) - 75,
+                left: center.x + radius * Math.cos(angle) - 75
+            }
+        }
+    });
+}
+
 function SideButtons() {
     const { setGameState } = useContext(GameContext) as GameContextType;
 
@@ -25,6 +61,15 @@ function SideButtons() {
             return {
                 ...oldState,
                 playerTokens: shuffleTokens(oldState.playerTokens)
+            }
+        });
+    }
+
+    function spread() {
+        setGameState(oldState => {
+            return {
+                ...oldState,
+                playerTokens: spreadTokens(oldState.playerTokens)
             }
         });
     }
@@ -50,6 +95,11 @@ function SideButtons() {
                 className="SideButtons__button General__backgroundImage SideButtons__shuffle"
                 style={{backgroundImage: "url(assets/shuffle.svg)"}}
                 onClick={shuffle}
+            ></div>
+            <div
+                className="SideButtons__button General__backgroundImage SideButtons__spread"
+                style={{backgroundImage: "url(assets/spread.svg)"}}
+                onClick={spread}
             ></div>
         </div>
     )
