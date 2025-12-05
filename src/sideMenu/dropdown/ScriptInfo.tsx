70575a149c3770d4bead41e4a427acf7d3ecf93e
Mirai Day <55056986+AD417@users.noreply.github.com>
2025-12-05 15:52:22 -0500

    Allow script author editing, to an extent

--- src/sideMenu/dropdown/ScriptInfo.tsx
+++ src/sideMenu/dropdown/ScriptInfo.tsx
@@ -8,6 +8,7 @@ import { ALL_SCRIPTS, SCRIPT_COLORS } from "../../data/scriptData";
 export default function ScriptInfo() {
     const { gameState, setGameState } = useContext(GameContext) as AppContextType & GameContextType;
     const titleRef = useRef<any>(null);
+    const authorRef = useRef<any>(null);
     
     const index = ALL_SCRIPTS.map(s => s[0].name.trim()).indexOf(gameState.script[0].name.trim());
     const color = SCRIPT_COLORS[index] ?? SCRIPT_COLORS[5];
@@ -29,17 +30,49 @@ export default function ScriptInfo() {
             }
         })
     }
+
+    function editAuthor() {
+        if (authorRef.current === null) return;
+        let author: string = authorRef.current.value;
+        if (author.startsWith("By: ")) {
+            author = author.slice(4);
+        }
+        console.log(`INPUT: "${titleRef.current.value}"`)
+        ALL_SCRIPTS[index][0].author = author;
+        setGameState(state => {
+            return {
+                ...state,
+                script: [
+                    {
+                        ...state.script[0],
+                        author: author
+                    },
+                    ...state.script.slice(1)
+                ]
+            }
+        })
+    }
+
     console.log(`SCRIPT NAME: "${ALL_SCRIPTS[index]?.[0]?.name}"`)
 
 
-    const authorJsx = [undefined, "undefined", ""].includes(gameState.script[0].author) ? 
-            <></> : 
-            <p style={{ color }} className="SideDropdown__scriptAuthor" >{"By: " + gameState.script[0].author}</p>;
+    const authorJsx = (
+            <TextareaAutosize 
+                ref={authorRef}
+                value={[undefined, "undefined", ""].includes(gameState.script[0].author) ? "By: unknown" : "By: " + gameState.script[0].author}
+                spellCheck="false"
+                style={{ color }} 
+                className="SideDropdown__scriptAuthor"
+                disabled={index < 6}
+                onChange={editAuthor}
+            />
+    );
     return (
         <>
             <TextareaAutosize 
-                value={gameState.script[0].name}
                 ref={titleRef}
+                value={gameState.script[0].name}
+                spellCheck="false"
                 style={{ color }} 
                 className="SideDropdown__scriptName" 
                 disabled={index < 6}
