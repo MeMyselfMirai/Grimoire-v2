a96c881a55ecd54c59f657c72f1f2195dba64230
Mirai Day <55056986+AD417@users.noreply.github.com>
2025-12-05 17:21:47 -0500

    force the port to be 8080

--- src/bottomButtons/ToggleVisibility.tsx
+++ src/bottomButtons/ToggleVisibility.tsx
@@ -6,7 +6,10 @@ import { GameContext, GameContextType } from "../data/gameState";
  * A button to toggle visibility. 
  * In the visible state, all token and reminder information will be freely
  * displayed to the user. 
- * In the invisible, or "town saure" state, only the aliveness of assigned tokens will be shown to the user. All other functionality will be disabled. This way, the Storyteller can display 
+ * In the invisible, or "town saure" state, only the aliveness of assigned
+ * tokens will be shown to the user. All other functionality will be disabled.
+ * This way, the Storyteller can display the aliveness of players without showing all of the information.
+ * 
  * @returns 
  */
 export default function ToggleVisibility() {
